b8afb31adcd7790e7b3fbb67a48ccd83d594b05f
Pratharan Sai Rupak Reddy <50819737+rupakreddy11@users.noreply.github.com>
2022-12-17 20:04:20 -0500

    Updated Explore.js

---
+++ explore.js
@@ -0,0 +1,13 @@
+
+function reveal(){
+    $(".pageTittleHeading").slideDown(3500);
+    $(".images").fadeIn(4000);
+    $(".images2").fadeIn(8000);
+    $(".images3").fadeIn(10000);
+    $(".images4").fadeIn(14000);
+    $(".images5").fadeIn(18000);
+
+    
+  };
+
+  reveal();
